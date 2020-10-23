1cc53a1df26c76fb981eb0e2cd4135da660c528a
Hyungi's portfolio <mike.hyungi.lee@gmail.com>
2020-10-23 21:06:54 +0900

    [update] wrap a REST API with GraphQL.

---
+++ graphql/resolvers.js
@@ -0,0 +1,21 @@
+// import { getMovies } from "./db";
+
+// const resolvers = {
+//     Query: {
+//         movies: (_, { rating, limit }) => getMovies(limit, rating)
+//     }
+// };
+
+// export default resolvers;
+
+import { getMovies, getMovie, getSuggestions } from "./db";
+
+const resolvers = {
+    Query: {
+        movies: (_, { rating, limit }) => getMovies(limit, rating),
+        movie: (_, { id }) => getMovie(id),
+        suggestions: (_, { id }) => getSuggestions(id)
+    }
+};
+
+export default resolvers;
